e707ce6d22c4aeda75141adee3fe9544b6918dbe
collnell <cnell@usgs.gov>
2021-07-12 17:25:18 -0500

    remove double download function

--- 1_fetch/src/get_nwis_data.R
+++ 1_fetch/src/get_nwis_data.R
@@ -1,31 +1,11 @@
-
-download_nwis_data <- function(site_num){
-  
-  # create the file names that are needed for download_nwis_site_data
-  # tempdir() creates a temporary directory that is wiped out when you start a new R session; 
-  # replace tempdir() with "1_fetch/out" or another desired folder if you want to retain the download
-  download_file <- file.path(tempdir(), paste0('nwis_', site_num, '_data.csv'))
-  download_nwis_site_data(download_file, parameterCd = '00010')
-  data_out <- read_csv(download_file, col_types = 'ccTdcc')
-  return(data_out)
-
-}
-
-nwis_site_info <- function(fileout, ...){
+nwis_site_info <- function(fileout, site_data){
   site_no <- unique(site_data$site_no)
   site_info <- dataRetrieval::readNWISsite(site_no)
   write_csv(site_info, fileout)
   return(fileout)
 }
 
-
-download_nwis_site_data <- function(filepath, parameterCd = '00010', startDate="2014-05-01", endDate="2015-05-01"){
-  
-  # filepaths look something like directory/nwis_01432160_data.csv,
-  # remove the directory with basename() and extract the 01432160 with the regular expression match
-  site_num <- basename(filepath) %>% 
-    stringr::str_extract(pattern = "(?:[0-9]+)")
-  
+download_nwis_site_data <- function(site_num, parameterCd = '00010', startDate="2014-05-01", endDate="2015-05-01"){
   # readNWISdata is from the dataRetrieval package
   data_out <- readNWISdata(sites=site_num, service="iv", 
                            parameterCd = parameterCd, startDate = startDate, endDate = endDate)
@@ -37,7 +17,7 @@ download_nwis_site_data <- function(filepath, parameterCd = '00010', startDate="
   }
   # -- end of do-not-edit block
   
-  write_csv(data_out, file = filepath)
-  return(filepath)
+  return(data_out)
+
 }
 
