ef6a86a618aaaf6cdcd6e19a3162a4b302fc4b0d
collnell <cnell@usgs.gov>
2021-07-12 17:18:42 -0500

    drop extra row binding step

--- 1_fetch/src/get_nwis_data.R
+++ 1_fetch/src/get_nwis_data.R
@@ -12,7 +12,6 @@ download_nwis_data <- function(site_num){
 }
 
 nwis_site_info <- function(fileout, ...){
-  site_data <- bind_rows(...)
   site_no <- unique(site_data$site_no)
   site_info <- dataRetrieval::readNWISsite(site_no)
   write_csv(site_info, fileout)
